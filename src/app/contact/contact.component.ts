fa151f286708bca28af8dac35dabeee497ba0304
Kappamille <camille.duquesne@cri-paris.com>
2021-11-01 23:59:33 +0100

    added pictures and links to projects

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  redirect(to: string): void {
+    if (to === 'github') {
+      window.location.href = 'https://github.com/Kappamille';
+    } else if (to === 'mail') {
+      window.location.href = 'mailto:camille.duquesne@cri-paris.org';
+    } else if (to === 'kaggle') {
+      window.location.href = 'https://www.kaggle.com/kappamille';
+    } else if (to === 'linkedin') {
+      window.location.href = 'https://www.linkedin.com/in/camilleduquesne/';
+    } else if (to === 'stackoverflow') {
+      window.location.href =
+        'https://stackoverflow.com/users/15514721/kappamille';
+    }
   }
-
 }
--- src/app/content.ts
+++ src/app/content.ts
@@ -29,7 +29,7 @@ export const PROJECT_LIST = [
     title: 'Building a ML model to predict Stroke',
     description:
       'This classification problem allowed me to practice skills such as: EDA, Data Vizualisation, Model Selection, Feature Selection, ... ',
-    image: 'assets/Background.jpg',
+    image: 'assets/stroke.jpg',
     date: '05.04.2021',
     link: '',
     tags: ['Data Science', 'Work in Progress'],
@@ -38,7 +38,7 @@ export const PROJECT_LIST = [
     title: 'Data Litteracy Course',
     description:
       'This 55h course was developped for the third year students of a Bachelor of Web Developement. It is an introduction to all main topics around data science (EDA, data visualisation, statistics, supervised ML, data collection, ethics and data storytelling )in order for the student to become data literate',
-    image: 'assets/Background.jpg',
+    image: 'assets/data.jpg',
     date: '01.09.2021',
     link: 'https://github.com/Kappamille/Python-DATA-P2022',
     tags: ['Data Science', 'Teaching', 'Ressource'],
@@ -47,7 +47,7 @@ export const PROJECT_LIST = [
     title: 'Python Course',
     description:
       "This 30h course was developed for the bootcamp of a master's program. This course allows beginner students to master the main concepts in python and shell to be asble to write simple scripts/algorithms.",
-    image: 'assets/Background.jpg',
+    image: 'assets/python.jpg',
     date: '01.09.2021',
     link: '',
     tags: ['Teaching', 'Ressource'],
@@ -56,7 +56,7 @@ export const PROJECT_LIST = [
     title: 'SQL Course',
     description:
       'This 55h course was developped for the third year students of a Bachelor of Web Developement. This course covers the main SQL concepts for student to be fluent in DQL, DDL, DML, DCL and TCL.',
-    image: 'assets/Background.jpg',
+    image: 'assets/database.jpg',
     date: '01.09.2021',
     link: 'https://github.com/Kappamille/Tech-Code-SQL-Avance',
     tags: ['Teaching', 'Ressource'],
